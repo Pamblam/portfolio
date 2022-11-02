3091e465a4d10cd99123e9279d852c1eebcc1075
Pamblam <adelphia@protonmail.ch>
2022-11-02 13:22:21 -0400

    saving

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-document.getElementById("toc-btn").addEventListener('click', e=>{
-	e.preventDefault();
-	document.getElementById("toc-container").scrollIntoView({ behavior: 'smooth', block: 'end'})
-});
+(async ()=>{
+
+	document.getElementById("toc-btn").addEventListener('click', e=>{
+		e.preventDefault();
+		document.getElementById("toc-container").scrollIntoView({ behavior: 'smooth'});
+	});
+
+	document.querySelectorAll('[data-scrollto]').forEach(ele=>{
+		ele.addEventListener('click', function(e){
+			e.preventDefault();
+			document.getElementById(this.dataset.scrollto).scrollIntoView({ behavior: 'smooth'});
+		});
+	});
+	
+	var em = 'adelphia' + '@' + 'protonmail' + '.' + 'ch';
+	var ph = '(813)' + ' 616' + '-0819';
+	document.getElementById("phone-span").innerHTML = `<a href='tel:${ph}'>${ph}</a>`;
+	document.getElementById("email-span").innerHTML = `<a href='mailto:${em}'>${em}</a>`;
+	
+})();
